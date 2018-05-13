907504da3eb05fa594aa5d46fad0cbf36c1f558c
rangerbboy <rangerbboy@github.com>
2018-05-13 13:28:28 +0700

    asdasd

--- app/src/main/java/com/example/cpd/ehutech/SV5TActivity.java
+++ app/src/main/java/com/example/cpd/ehutech/SV5TActivity.java
@@ -2,6 +2,7 @@ package com.example.cpd.ehutech;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class SV5TActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class SV5TActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sv5_t);
+        EditText mMssvView;
     }
 }
