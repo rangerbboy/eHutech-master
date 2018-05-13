d716ff3cf0abfea6f207f2a27dab4742fbb68905
rangerbboy <rangerbboy@github.com>
2018-05-13 13:31:03 +0700

    test

--- app/src/main/java/com/example/cpd/ehutech/SV5TActivity.java
+++ app/src/main/java/com/example/cpd/ehutech/SV5TActivity.java
@@ -11,5 +11,6 @@ public class SV5TActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sv5_t);
         EditText mMssvView;
+        EditText masa;
     }
 }
